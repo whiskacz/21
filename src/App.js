6cfcf5b03243958d0e24455c2bdf65fb1077071d
whiskacz <pawelblasiak1985@gmail.com>
2023-03-24 12:22:34 +0100

    React createRef exercise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import React from 'react';
+
+
+class App extends React.Component {
+  text = React.createRef();
+  render(){
+  // const [counterA, setcounterA] = useState(0)
+  // const [counterB, setcounterB] = useState(0)
+
+  // const handlerA = () => setcounterA(counterA + 1)
+  // const handlerB = () => setcounterB(counterB + 1)
+
+  // const [toggler, settoggler] = useState(true)
+
+
+ 
+
+ 
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <span>a wynosi {counterA}</span>
+      <br />
+      <span>b wynosi {counterB}</span>
+      <br />
+
+      <button onClick={handlerA}>klik A</button>
+      <button onClick={handlerB}>klik B</button> */}
+      <br />
+      <br />
+      <br />
+      <button onClick={this.togglerAdd}>toggler </button>
+      <br />
+      <br />
+      <br />
+      <div ref={this.text}> Kocham Cie </div>
+
+
     </div>
   );
+  
 }
+togglerAdd = () => 
+  this.text.current.textContent += "!"
+}
+
+
+
+// const test = () => {
+//   return(
+//     <div>
+//       <p>{counter}</p>
+//       <p>{rerenderCounter}</p>
+//     </div>
+//   )
+// }
 
 export default App;
